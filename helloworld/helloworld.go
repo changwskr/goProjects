1d18eace5ebcae0b2e096da329bf305aae7330f9
changwskr <changwskr@gmail.com>
2021-06-19 01:09:35 +0900

    7 commit

--- helloworld/helloworld.go
+++ helloworld/helloworld.go
@@ -5,13 +5,9 @@ func main() {
 	fmt.Println("hello world")	
 }
 
-/*
-1. 실행하가
-PS D:\z.study\goProjects> go run helloworld.go
-hello world
-2. 컴파일
-
-PS D:\z.study\goProjects> go build
-PS D:\z.study\goProjects> go mod init goprojects/helloworld
-PS D:\z.study\goProjects> go build
-*/
+// 1. 실행하가
+// PS D:\z.study\goProjects> go run helloworld.go
+// 2. 컴파일
+// PS D:\z.study\goProjects> go build
+// PS D:\z.study\goProjects> go mod init goprojects/helloworld
+// PS D:\z.study\goProjects> go build
